92925ab0ebb39d6208898c2be9f05f8e733e839b
nz-bill <bill.palmestedt@zocom.se>
2024-11-07 09:49:48 +0100

    Added line to printMenu()

--- src/Game.java
+++ src/Game.java
@@ -29,6 +29,7 @@ public class Game {
     }
 
     public void printMenu(){
+        System.out.println("-----Meny------");
         System.out.println("vad vill du göra?");
         System.out.println("1. slåss");
         System.out.println("0. avsluta");
